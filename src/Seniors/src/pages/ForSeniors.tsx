7b20aad71e56f9bad69cf3efa8b4e153a29f7c5f
AI VECTORS <103539434+AIV-GH@users.noreply.github.com>
2022-10-27 03:20:43 -0700

    Add files via upload

---
+++ src/Seniors/src/pages/ForSeniors.tsx
@@ -0,0 +1,253 @@
+import { FunctionComponent } from "react";
+import styles from "./ForSeniors.module.css";
+import { Link } from "react-router-dom";
+
+const ForSeniors: FunctionComponent = () => {
+  return (
+    <>
+      <div className={`${styles.forSeniorsDiv} container`}>
+        <section className={`${styles.topNav} container`}>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarTogglerDemo01"
+                aria-controls="navbarTogglerDemo01"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div
+                className="collapse navbar-collapse"
+                id="navbarTogglerDemo01"
+              >
+                <a className="navbar-brand" href="/">
+                  <div className={styles.logomain}>
+                    <img src="chatbot.png" height="50" width="50" />
+                  </div>
+                </a>
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex justify-content-center align-items-center">
+                  <li className={styles.listDiv}>
+                    <div className={styles.sENIORSDiv}>SENIORS</div>
+                  </li>
+                  <li className={styles.listDiv}>
+                    <Link to="/product">
+                      <div className={styles.pRODUCTDiv}>PRODUCT</div>
+                    </Link>
+                  </li>
+                  <li className={styles.listDiv}>
+                    {" "}
+                    <Link to="/competitors">
+                      <div className={styles.cOMPETITORSDiv}>COMPETITORS</div>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </section>
+
+        <section>
+          <div className="row mt-5">
+            <div className="col-sm-12 col-xs1-12 col-md-6">
+              <b className={styles.aICaregiverForElders}>
+                AI Caregiver for Elders
+              </b>
+              <p className={styles.usingOptimusToAvoidInciden}>
+                Using Optimus to avoid incidents and monitor behaviour patterns
+                for unusual behaviour in elders allows many seniors to stay in
+                their homes safely. And notifying caregivers, family, friends
+                and doctors for quick assistance to elders.
+              </p>
+            </div>
+            <div className="col-sm-12 col-xs1-12 col-md-6">
+              <img className={styles.untitled11Icon} alt="" src="../123.png" />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="row">
+            <div className="col-sm-12 col-xs1-12 col-md-6">
+              <div className={styles.groupDiv17}>
+                <b className={styles.fallDetectionB}>Fall Detection</b>
+
+                <p
+                  className={styles.fallsAreTheLeadingCauseOf}
+                >{`Falls are the leading cause of fatal and nonfatal injuries among adults aged ≥65 years (older adults). Our solution is to monitor elders 24/7 and detect in case of falls only by using images from a standard video-camera without the need to use depth camera nor environmental sensors. `}</p>
+              </div>
+              <div>
+                <img
+                  className={styles.w023N0032929bP12929ConveIcon}
+                  alt=""
+                  src="../img_2208w023n003.svg"
+                />
+              </div>
+            </div>
+
+            <div className="col-sm-12 col-xs1-12 col-md-6">
+              <div className={styles.groupDiv21}>
+                <b className={styles.sleepBehaviour}>Medication Schedule</b>
+
+                <p
+                  className={styles.optimusIsACuttingEdgeVirt}
+                >{`Optimus voice alerts seniors about medication, and optimus camera detects medication consumption and notifies care taker if the user is missing a schedule. `}</p>
+              </div>
+              <div>
+                <img
+                  className={styles.oldWomanWithMigraineTakingIcon}
+                  alt=""
+                  src="../group.svg"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="row mt-5">
+            <div className="col-sm-12 col-xs1-12 col-md-6">
+              <div className={styles.groupDiv18}>
+                <b className={styles.sleepBehaviour}>Sleep Behaviour</b>
+
+                <div
+                  className={styles.optimusIsACuttingEdgeVirt}
+                >{`Optimus camera detects senior sleep pattern and analyses and sends notification to care taker on daily or weekly. `}</div>
+              </div>
+              <div>
+                {" "}
+                <img
+                  className={styles.oldWomanSufferingFromHeadaIcon}
+                  alt=""
+                  src="../oldwomansufferingfromheadacheinhospital-1.svg"
+                />
+              </div>
+            </div>
+
+            <div className="col-sm-12 col-xs1-12 col-md-6">
+              <div className={styles.groupDiv22}>
+                <b className={styles.sleepBehaviour}>Prediction Anomalies</b>
+
+                <p
+                  className={styles.optimusIsACuttingEdgeVirt}
+                >{`Optimus eyes detects senior anomaly behaviors and notifies care taker if necessary. `}</p>
+              </div>
+              <div>
+                <img
+                  className={styles.retiredWomenFellDownByHeaIcon}
+                  alt=""
+                  src="../retiredwomenfelldownbyheartattack-1.svg"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className={`${styles.topSpace} row`}>
+            <div className="col-sm-12 col-xs-12 col-md-6">
+              <div className={styles.groupDiv19}>
+                <b className={styles.sleepBehaviour}>Vision </b>
+
+                <p
+                  className={styles.optimusIsACuttingEdgeVirt}
+                >{`Optimus eyes and voice helps low vision seniors by reading and speaking expiry dates of food packages, and many more. `}</p>
+              </div>
+              <div>
+                <img
+                  className={styles.oldPersonSmoking11}
+                  alt=""
+                  src="../oldpersonsmoking-1-1.svg"
+                />
+              </div>
+              <div>
+                <div className={styles.groupDiv20}>
+                  <b className={styles.sleepBehaviour}>Transportation</b>
+
+                  <p
+                    className={styles.optimusIsACuttingEdgeVirt}
+                  >{`Seniors using tech is not quite often, so Optimus custom skill set (Amazon Alexa) helps checking route, calling cab and many more with simple questions. `}</p>
+                </div>
+                <div>
+                  <img
+                    className={styles.icon}
+                    alt=""
+                    src="../3648586-1@2x.png"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-12 col-xs-12 col-md-6">
+              <div className={styles.groupDiv23}>
+                <b className={styles.sleepBehaviour}>Nutrition</b>
+
+                <p
+                  className={styles.optimusIsACuttingEdgeVirt}
+                >{`Optimus custom NLP skill set(Amazon Alexa) for seniors, helps understanding nutritions. `}</p>
+              </div>
+              <div>
+                <img className={styles.icon1} alt="" src="../2565-1.svg" />
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <section className={styles.footerDivs}>
+        <div className={`${styles.footerDiv} container`}>
+          {/* <div className={styles.rectangleDiv} />
+          <div className={styles.rectangleDiv1} /> */}
+          <div className="row">
+            <div className="col-sm-12 col-xs-12 col-md-3">
+              <a href="https://www.aitensors.org/">
+                <div className={styles.logoDiv}>
+                  <img src="chatbot1.png" height="50" width="50" />{" "}
+                </div>
+              </a>
+              <div className={styles.transformingLivesOfPeopleA}>
+                Transforming lives across the globe by adventuring Artificial
+                Intelligence.
+              </div>
+            </div>
+            <div className="col-sm-12 col-xs-12 col-md-3">
+              <div className={styles.linksDiv}>Links</div>
+              <div className={styles.companyDiv}>Company</div>
+              <Link to="/product">
+                <div className={styles.productDiv}>Product</div>
+              </Link>
+              <Link to="/competitors">
+                <div className={styles.competitorsDiv}>Competitors</div>
+              </Link>
+              <div className={styles.pricingDiv}>Pricing</div>
+              <div className={styles.contactDiv}>Contact</div>
+            </div>
+            <div className="col-sm-12 col-xs-12 col-md-3">
+              <div className={styles.followUsDiv}>{`Follow Us `}</div>
+              <div className={styles.groupDiv}>
+                <a href="https://twitter.com/AiTensors">
+                  <div className={styles.fbcomaaaDiv}>Twitter</div>
+                </a>
+              </div>
+              <div className={styles.groupDiv2}>
+                <a href="https://www.linkedin.com/company/82789169/">
+                  <div className={styles.fbcomaaaDiv}>Linkedin</div>
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-12 col-xs-12 col-md-3">
+              <div className={styles.getInTouchWithUs}>
+                Get In touch with Us
+              </div>
+              <div
+                className={styles.needAnswersNeedHelpJust}
+              >{`Need Answers? Need help ? Just email us `}</div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className={styles.copyright2022Div}>Copyright 2022</div>
+    </>
+  );
+};
+
+export default ForSeniors;
---
+++ src/payment/src/Payment2.tsx
@@ -0,0 +1,273 @@
+import { FunctionComponent, useCallback } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import styles from "./css/Payment2.module.css";
+
+export const Payment2: FunctionComponent = () => {
+  const navigate = useNavigate();
+
+  const onGroupContainer22Click = useCallback(() => {
+    navigate("/payment1");
+  }, [navigate]);
+
+  const onRectangleRectangle23Click = useCallback(() => {
+    const anchor = document.querySelector(
+      "[data-scroll-to='rectangleRectangle']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  }, []);
+
+  const onElementsButtonsPrimaryClick = useCallback(() => {
+    navigate("/downloadscreen");
+  }, [navigate]);
+
+  const onCOMPETITORSTextClick = useCallback(() => {
+    // Please sync "Competitors" to the project
+  }, []);
+
+  const onLOGOTextClick = useCallback(() => {
+    // Please sync "Page 1 Home" to the project
+  }, []);
+
+  return (
+    <div className={styles.paymentDiv}>
+      <div className={styles.rectangleDiv} />
+      <div className={styles.groupDiv}>
+        <div className={styles.rectangleDiv1} />
+        <div className={styles.rectangleDiv2} />
+        <div className={styles.rectangleDiv3} />
+        <div className={styles.rectangleDiv4} />
+        <div className={styles.rectangleDiv5} />
+        <div className={styles.rectangleDiv6} />
+      </div>
+      <div className={styles.homeBannerDiv}>
+        <div className={styles.rectangleDiv7} />
+        <img className={styles.ellipseIcon} alt="" src="ellipse-42.svg" />
+        <img className={styles.groupIcon} alt="" src="group-163.svg" />
+        <div className={styles.groupDiv1}>
+          <div className={styles.image48Div} />
+          <img className={styles.groupIcon1} alt="" src="group-254.svg" />
+          <div className={styles.groupDiv2}>
+            <img className={styles.iconlyLightNotification} alt="" />
+            <div className={styles.jumpStartDiv}>{`Jump start `}</div>
+            <div className={styles.yourPortfolioLooksGreatTod}>
+              Your portfolio looks great today
+            </div>
+          </div>
+          <div className={styles.groupDiv3}>
+            <div className={styles.groupDiv4}>
+              <div className={styles.portfolioBalanceDiv}>
+                Portfolio balance
+              </div>
+              <div className={styles.div}>$12,175.11</div>
+              <div className={styles.groupDiv5}>
+                <img className={styles.vectorIcon} alt="" src="vector-1.svg" />
+                <div className={styles.div1}>$24.74 (3.79%)</div>
+              </div>
+              <div className={styles.todayMay222021}>Today (May 22,2021)</div>
+            </div>
+            <div className={styles.groupDiv6}>
+              <div className={styles.availableBalanceDiv}>
+                Available balance
+              </div>
+              <div className={styles.div2}>$173.21</div>
+              <div className={styles.groupDiv7}>
+                <img className={styles.vectorIcon1} alt="" />
+                <div className={styles.portfolioBalanceDiv}>More details</div>
+              </div>
+            </div>
+            <div className={styles.groupDiv8}>
+              <div className={styles.rectangleDiv8} />
+              <div className={styles.hDiv}>1h</div>
+            </div>
+            <div className={styles.groupDiv9}>
+              <div className={styles.rectangleDiv9} />
+              <div className={styles.hDiv1}>24h</div>
+            </div>
+            <div className={styles.groupDiv10}>
+              <div className={styles.rectangleDiv9} />
+              <div className={styles.wDiv}>1w</div>
+            </div>
+            <div className={styles.groupDiv11}>
+              <div className={styles.rectangleDiv9} />
+              <div className={styles.wDiv}>1w</div>
+            </div>
+            <div className={styles.groupDiv12}>
+              <div className={styles.rectangleDiv9} />
+              <div className={styles.yDiv}>1y</div>
+            </div>
+            <div className={styles.groupDiv13}>
+              <div className={styles.rectangleDiv9} />
+              <div className={styles.wDiv}>All</div>
+            </div>
+            <div className={styles.rectangleDiv14} />
+            <img className={styles.groupIcon2} alt="" src="group-18.svg" />
+            <div className={styles.groupDiv14}>
+              <div className={styles.rectangleDiv15} />
+              <div className={styles.sendDiv}>Send</div>
+            </div>
+            <div className={styles.groupDiv15}>
+              <div className={styles.rectangleDiv16} />
+              <div className={styles.withdrawDiv}>Withdraw</div>
+            </div>
+            <img className={styles.groupIcon3} alt="" />
+            <img className={styles.groupIcon4} alt="" src="group-274.svg" />
+          </div>
+        </div>
+        <img className={styles.bluee011Icon} alt="" src="bluee01-1@2x.png" />
+      </div>
+      <img className={styles.ezgif1Icon} alt="" src="ezgif-1@2x.png" />
+      <img className={styles.ezgif3Icon} alt="" src="ezgif-1@2x.png" />
+      <img className={styles.ezgif4Icon} alt="" src="ezgif-4@2x.png" />
+      <div className={styles.groupDiv16}>
+        <img className={styles.ezgif6Icon} alt="" src="ezgif-4@2x.png" />
+      </div>
+      <div className={styles.footerDiv}>
+        <div className={styles.rectangleDiv17} />
+        <div className={styles.rectangleDiv18} />
+        <div className={styles.logoDiv}><img src="logo.png" height="50" width="50" /></div>
+        <div className={styles.transformingLivesOfPeopleA}>
+        Transforming lives across the globe by adventuring Artificial Intelligence.
+        </div>
+        <div
+          className={styles.needAnswersNeedHelpJust}
+        >{`Need Answers? Need help ? Just email us `}</div>
+        <div className={styles.copyright2022Div}>Copyright 2022</div>
+        <div className={styles.companyDiv}>Company</div>
+        <Link to="/product"><div className={styles.productDiv}>Product</div></Link>
+        <Link to="/competitors"><div className={styles.competitorsDiv}>Competitors</div></Link>
+        <div className={styles.pricingDiv}>Pricing</div>
+        <div className={styles.contactDiv}>Contact</div>
+        <div className={styles.linksDiv}>Links</div>
+        <div className={styles.followUsDiv}>{`Follow Us `}</div>
+        <div className={styles.getInTouchWithUs}>Get In touch with Us</div>
+        <div className={styles.groupDiv17}>
+          <div className={styles.fbcomaaaDiv}>https://twitter.com/AiTensors</div>
+            <a href="https://twitter.com/AiTensors"><img
+              className={styles.featherfacebookIcon}
+              alt=""
+              src="twitter.svg"
+            /></a>
+        </div>
+        <div className={styles.groupDiv19}>
+          <div className={styles.fbcomaaaDiv}>https://www.linkedin.com/company/82789169/</div>
+            <a href="https://www.linkedin.com/company/82789169/"><img
+              className={styles.featherfacebookIcon}
+              alt=""
+              src="linkedin.svg"
+            /></a>
+        </div>
+        
+      </div>
+      <div className={styles.categoryDiv}>
+        <div className={styles.frameDiv1}>
+          <div className={styles.helperTextDiv}>Free</div>
+          <b className={styles.b}>$0</b>
+        </div>
+        <div className={styles.frameDiv2}>
+          <div className={styles.pointsDiv}>
+            <img className={styles.feathersendIcon} alt="" />
+            <div className={styles.visitorsDiv}>Loreum</div>
+          </div>
+          <div className={styles.pointsDiv}>
+            <img className={styles.feathersendIcon} alt="" />
+            <div className={styles.visitorsDiv}>Ipsum</div>
+          </div>
+          <div className={styles.pointsDiv}>
+            <img className={styles.feathersendIcon} alt="" />
+            <div className={styles.visitorsDiv}>Dolor</div>
+          </div>
+        </div>
+        <div className={styles.buttonDiv}>
+          <div className={styles.downloadNowDiv}>Download Now</div>
+        </div>
+      </div>
+      
+      <b className={styles.trackAnyHashtags}>Choose your payment method</b>
+      <div className={styles.groupDiv20}>
+        <div className={styles.rectangleDiv20} />
+      </div>
+      <b className={styles.premimumPlanWorth99WasSe}>
+      Premium Plan is Ready
+      </b>
+      <div className={styles.groupDiv21}>
+        <div className={styles.groupDiv22}>
+          <div className={styles.rectangleDiv21} />
+          <b className={styles.debitCreditCard}>Debit /Credit Card</b>
+        </div>
+        <div className={styles.groupDiv23} onClick={onGroupContainer22Click}>
+          <div className={styles.rectangleDiv21} />
+          <b className={styles.debitCreditCard}>Wallet</b>
+        </div>
+        <div className={styles.groupDiv24}>
+          <div
+            className={styles.rectangleDiv21}
+            data-scroll-to="rectangleRectangle"
+          />
+          <b className={styles.debitCreditCard}>Net Banking</b>
+        </div>
+        <div
+          className={styles.rectangleDiv24}
+          onClick={onRectangleRectangle23Click}
+        />
+        <div className={styles.formDiv}>
+          <div className={styles.buttonDiv1}>
+            <div
+              className={styles.elementsButtonsPrimary}
+              onClick={onElementsButtonsPrimaryClick}
+            >
+              <div className={styles.inputDiv}>
+                <div className={styles.baseDiv} />
+                <div className={styles.buttonTextDiv}>Proceed</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.formDiv1}>
+            <div className={styles.elementsInputsLarge60px}>
+              <div className={styles.inputDiv}>
+                <div className={styles.baseDiv1} />
+                <div className={styles.typeYourEMail}>Bank2</div>
+              </div>
+            </div>
+            <div className={styles.elementsInputsLarge60px1}>
+              <div className={styles.inputDiv}>
+                <div className={styles.baseDiv1} />
+                <div className={styles.typeYourEMail}>Bank1</div>
+              </div>
+            </div>
+            <div className={styles.typeYourEMail2}>Note :</div>
+            <div className={styles.elementsInputsLarge60px2}>
+              <div className={styles.inputDiv}>
+                <div className={styles.baseDiv1} />
+                <div className={styles.typeYourEMail}>Bank 3</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.typeYourEMail4}>
+            Is billing address same as residence address
+          </div>
+          <div className={styles.rectangleDiv25} />
+        </div>
+      </div>
+      <div className={styles.registrationPayment}>
+        <span>{`Registration -   `}</span>
+        <span className={styles.paymentSpan}>Payment</span>
+        <span> - Download</span>
+      </div>
+      <div className={styles.navBarDiv}>
+        <div className={styles.cONTACTDiv}>CONTACT</div>
+        <div className={styles.pRICINGDiv}>PRICING</div>
+        <div className={styles.tESTIMONIALSDiv}>TESTIMONIALS</div>
+        <div className={styles.cOMPETITORSDiv} onClick={onCOMPETITORSTextClick}>
+          COMPETITORS
+        </div>
+        <div className={styles.pRODUCTDiv}>PRODUCT</div>
+        <div className={styles.cOMPANYDiv}>COMPANY</div>
+        <Link to="/"><div className={styles.logomain}>
+          <img src="logo.png" height="50" width="50" />
+        </div></Link>
+      </div>
+    </div>
+  );
+};
